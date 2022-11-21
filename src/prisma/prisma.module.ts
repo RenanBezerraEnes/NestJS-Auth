503e1803f423579479f564394554330e5161e400
Renan Bezerra <renanbezerraenes@icloud.com>
2022-11-21 15:48:07 +0100

    create user + password bcrypt

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  controllers: [PrismaService],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
